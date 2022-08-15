d6d4ddf64f34d13fe7e2b39444b2652f2032aae3
Bhaskar <panthribhaskar@gmail.com>
2022-08-15 23:00:55 +0530

    after adding content

--- medStore/views.py
+++ medStore/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from .models import *
 
 def registerPage(request):
     form = UserCreationForm()
@@ -13,17 +14,50 @@ def registerPage(request):
     return render(request, 'store/login.html',context)
 
 def home(request):
-    context={}
+    products = Product.objects.all()
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer = customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+    context={'products' : products, 'items':items, 'order':order}
     return render(request, 'store/index.html', context)
 
 def shop(request):
-    context={}
+    products = Product.objects.all()
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer = customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+    context={'products' : products, 'items':items, 'order':order}
     return render(request, 'store/shop.html', context)
 
 def cart(request):
-    context={}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer = customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    context={'items':items, 'order':order}
     return render(request, 'store/cart.html',context)
 
 def checkout(request):
-    context={}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer = customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    context={'items':items, 'order':order}
     return render(request, 'store/checkout.html',context)
